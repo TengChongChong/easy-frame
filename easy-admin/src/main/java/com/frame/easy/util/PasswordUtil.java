c331197ef483c31b4e23a86cae048576d1f44e09
tengchong <tengchongchong@foxmail.com>
2019-01-26 16:21:21 +0800

    删除不用import

--- easy-admin/src/main/java/com/frame/easy/util/PasswordUtil.java
+++ easy-admin/src/main/java/com/frame/easy/util/PasswordUtil.java
@@ -1,10 +1,10 @@
 package com.frame.easy.util;
 
 import cn.hutool.crypto.SecureUtil;
-import cn.hutool.crypto.symmetric.SymmetricAlgorithm;
 
 /**
  * 密码工具
+ *
  * @author tengchong
  * @date 2018/9/4
  */
@@ -19,25 +19,28 @@ public class PasswordUtil {
 
     /**
      * 加密密码 用于登录
+     *
      * @param password 密码(经过一次md5)
-     * @param salt 盐
+     * @param salt     盐
      * @return 加密后密码
      */
-    public static String encryptedPasswords(String password, String salt){
+    public static String encryptedPasswords(String password, String salt) {
         return SecureUtil.md5((SecureUtil.md5(salt + UUID) + password));
     }
 
     /**
      * 生成密码 用于用户注册/新增用户
+     *
      * @param password 密码 未加密
-     * @param salt 盐
+     * @param salt     盐
      * @return 加密后密码
      */
-    public static String generatingPasswords(String password, String salt){
+    public static String generatingPasswords(String password, String salt) {
         return SecureUtil.md5((SecureUtil.md5(salt + UUID) + SecureUtil.md5(password)));
     }
 
-    public static void main(String[] arges){
-        byte[] key = SecureUtil.generateKey(SymmetricAlgorithm.AES.getValue()).getEncoded();
+    public static void main(String[] arges) {
+//        byte[] key = SecureUtil.generateKey(SymmetricAlgorithm.AES.getValue()).getEncoded();
+
     }
 }
