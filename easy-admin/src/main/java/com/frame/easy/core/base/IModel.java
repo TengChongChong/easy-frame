9d33266d4505a7725396f2bb4d95f5014c351de1
tengchong <tengchongchong@foxmail.com>
2019-01-31 14:09:55 +0800

    添加IModel

---
+++ easy-admin/src/main/java/com/frame/easy/core/base/IModel.java
@@ -0,0 +1,18 @@
+package com.frame.easy.core.base;
+
+import com.frame.easy.common.page.Page;
+
+/**
+ * 实体类接口
+ *
+ * @author tengchong
+ * @date 2019-01-31
+ */
+public interface IModel {
+    /**
+     * 获取分页信息
+     *
+     * @return page
+     */
+    Page getPage();
+}
