bb56d49fca4e3fa0c72d3f573f0693519b01540b
tengchong <tengchongchong@foxmail.com>
2019-03-11 10:16:04 +0800

    登录成功后设置头像缩略图路径

--- easy-admin/src/main/java/com/frame/easy/core/shiro/check/RetryLimitCredentialsMatcher.java
+++ easy-admin/src/main/java/com/frame/easy/core/shiro/check/RetryLimitCredentialsMatcher.java
@@ -8,6 +8,7 @@ import com.frame.easy.util.RedisUtil;
 import com.frame.easy.util.ShiroUtil;
 import com.frame.easy.modular.sys.model.SysUser;
 import com.frame.easy.modular.sys.service.ShiroService;
+import com.frame.easy.util.file.ImageUtil;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
 import org.slf4j.Logger;
@@ -34,8 +35,8 @@ public class RetryLimitCredentialsMatcher extends CredentialsMatcher {
 
     @Override
     public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
-        SysUser user = (SysUser) info.getPrincipals().getPrimaryPrincipal();
-        String loginCountKey = RedisPrefix.ACCOUNT + "login_count_" + user.getUsername();
+        SysUser sysUser = (SysUser) info.getPrincipals().getPrimaryPrincipal();
+        String loginCountKey = RedisPrefix.ACCOUNT + "login_count_" + sysUser.getUsername();
         // 登录尝试次数
         int loginCount = Integer.parseInt(String.valueOf(RedisUtil.get(loginCountKey)));
         // 剩余尝试次数
@@ -50,13 +51,18 @@ public class RetryLimitCredentialsMatcher extends CredentialsMatcher {
         // 清空登录计数
         RedisUtil.del(loginCountKey);
         // 更新最后登录时间
-        shiroService.updateUserLastLoginDate(user.getId());
+        shiroService.updateUserLastLoginDate(sysUser.getId());
         // 检查是否允许用户在多处登录
         if (true) {
-            shiroService.kickOutSession(user);
+            shiroService.kickOutSession(sysUser);
         }
         // 用户信息放在session里
-        ShiroUtil.setAttribute(SessionConst.USER_SESSION_KEY, user);
+        // 设置头像缩略图
+        sysUser.setAvatarLg(ImageUtil.getThumbnailUrlByUrl(sysUser.getAvatar(), ImageUtil.IMAGE_SIZE_LG));
+        sysUser.setAvatarMd(ImageUtil.getThumbnailUrlByUrl(sysUser.getAvatar(), ImageUtil.IMAGE_SIZE_MD));
+        sysUser.setAvatarSm(ImageUtil.getThumbnailUrlByUrl(sysUser.getAvatar(), ImageUtil.IMAGE_SIZE_SM));
+        sysUser.setAvatarXs(ImageUtil.getThumbnailUrlByUrl(sysUser.getAvatar(), ImageUtil.IMAGE_SIZE_XS));
+        ShiroUtil.setAttribute(SessionConst.USER_SESSION_KEY, sysUser);
         return true;
     }
 }
