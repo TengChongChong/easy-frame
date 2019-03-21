d78732c0f3c9f2c83227ee576f11c809338ced95
tengchong <tengchongchong@foxmail.com>
2019-03-21 13:22:04 +0800

    只允许开发模式生成代码

--- easy-admin/src/main/java/com/frame/easy/modular/generator/service/impl/GenerationServiceImpl.java
+++ easy-admin/src/main/java/com/frame/easy/modular/generator/service/impl/GenerationServiceImpl.java
@@ -5,10 +5,12 @@ import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.StrategyConfig;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.frame.easy.common.constant.CommonConst;
 import com.frame.easy.common.select.Select;
 import com.frame.easy.common.status.PermissionsStatus;
 import com.frame.easy.common.type.PermissionsType;
 import com.frame.easy.config.properties.DataSourceProperties;
+import com.frame.easy.exception.EasyException;
 import com.frame.easy.generator.config.GeneratorConfig;
 import com.frame.easy.generator.config.builder.ConfigBuilder;
 import com.frame.easy.modular.generator.dao.GenerationMapper;
@@ -51,53 +53,57 @@ public class GenerationServiceImpl implements GenerationService {
             object.setUsername(dataSourceProperties.getUsername());
             object.setPassword(dataSourceProperties.getPassword());
             object.init();
-            generatorConfig.generation(object);
-            // 检查是否需要添加菜单
-            if(StrUtil.isNotBlank(object.getMenuName())){
-                // 菜单名称不为空
-                // 检查菜单名称是否存在
-                if(!sysPermissionsService.checkMenuIsHaving(object.getMenuName())){
-                    // 菜单不存在
-                    SysPermissions basePermission = getNewMenu(object.getMenuName(),
-                            object.getPermissionsCode() + ":select",
-                            "/auth/" + object.getControllerMapping() + "/list",
-                            1);
-                    basePermission.setpId(0L);
-                    basePermission.setType(PermissionsType.ENABLE.getCode());
-                    sysPermissionsService.saveData(basePermission);
-                    if(StrUtil.isNotBlank(object.getPermissionsCode())){
-                        // 如果权限代码不为空,保存方法权限
-                        if(object.getGenSave()){
-                            SysPermissions savePermission = getNewMenu("保存/修改",
-                                    object.getPermissionsCode() + ":save",
-                                    null,
-                                    2);
-                            savePermission.setIcon("la la-save");
-                            savePermission.setpId(basePermission.getId());
-                            sysPermissionsService.saveData(savePermission);
-                        }
-                        if(object.getGenDelete()){
-                            SysPermissions savePermission = getNewMenu("删除",
-                                    object.getPermissionsCode() + ":delete",
-                                    null,
-                                    2);
-                            savePermission.setIcon("la la-trash");
-                            savePermission.setpId(basePermission.getId());
-                            sysPermissionsService.saveData(savePermission);
-                        }
-                        if(object.getGenAdd()){
-                            SysPermissions savePermission = getNewMenu("新增",
-                                    object.getPermissionsCode() + ":add",
-                                    null,
-                                    2);
-                            savePermission.setIcon("la la-plus");
-                            savePermission.setpId(basePermission.getId());
-                            sysPermissionsService.saveData(savePermission);
+            if (CommonConst.PROFILES_ACTIVE_DEV.equals(CommonConst.projectProperties.getProfilesActive())) {
+                generatorConfig.generation(object);
+                // 检查是否需要添加菜单
+                if (StrUtil.isNotBlank(object.getMenuName())) {
+                    // 菜单名称不为空
+                    // 检查菜单名称是否存在
+                    if (!sysPermissionsService.checkMenuIsHaving(object.getMenuName())) {
+                        // 菜单不存在
+                        SysPermissions basePermission = getNewMenu(object.getMenuName(),
+                                object.getPermissionsCode() + ":select",
+                                "/auth/" + object.getControllerMapping() + "/list",
+                                1);
+                        basePermission.setpId(0L);
+                        basePermission.setType(PermissionsType.ENABLE.getCode());
+                        sysPermissionsService.saveData(basePermission);
+                        if (StrUtil.isNotBlank(object.getPermissionsCode())) {
+                            // 如果权限代码不为空,保存方法权限
+                            if (object.getGenSave()) {
+                                SysPermissions savePermission = getNewMenu("保存/修改",
+                                        object.getPermissionsCode() + ":save",
+                                        null,
+                                        2);
+                                savePermission.setIcon("la la-save");
+                                savePermission.setpId(basePermission.getId());
+                                sysPermissionsService.saveData(savePermission);
+                            }
+                            if (object.getGenDelete()) {
+                                SysPermissions savePermission = getNewMenu("删除",
+                                        object.getPermissionsCode() + ":delete",
+                                        null,
+                                        2);
+                                savePermission.setIcon("la la-trash");
+                                savePermission.setpId(basePermission.getId());
+                                sysPermissionsService.saveData(savePermission);
+                            }
+                            if (object.getGenAdd()) {
+                                SysPermissions savePermission = getNewMenu("新增",
+                                        object.getPermissionsCode() + ":add",
+                                        null,
+                                        2);
+                                savePermission.setIcon("la la-plus");
+                                savePermission.setpId(basePermission.getId());
+                                sysPermissionsService.saveData(savePermission);
+                            }
                         }
                     }
                 }
+                return true;
+            } else {
+                throw new EasyException("当前模式[" + CommonConst.projectProperties.getProfilesActive() + "]不允许生成");
             }
-            return true;
         }
         throw new RuntimeException("参数获取失败");
     }
@@ -107,11 +113,11 @@ public class GenerationServiceImpl implements GenerationService {
      *
      * @param name 名称
      * @param code 权限代码
-     * @param url 访问地址
-     * @param lev 级别
+     * @param url  访问地址
+     * @param lev  级别
      * @return
      */
-    private SysPermissions getNewMenu(String name, String code, String url, int lev){
+    private SysPermissions getNewMenu(String name, String code, String url, int lev) {
         SysPermissions sysPermissions = new SysPermissions();
         sysPermissions.setName(name);
         sysPermissions.setCode(code);
