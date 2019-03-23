0fb7509ac7e0c08c30141648ea70d610c580a5aa
tengchong <tengchongchong@foxmail.com>
2019-03-23 15:18:46 +0800

    添加常量

--- easy-admin/src/main/java/com/frame/easy/common/constant/SessionConst.java
+++ easy-admin/src/main/java/com/frame/easy/common/constant/SessionConst.java
@@ -7,10 +7,12 @@ package com.frame.easy.common.constant;
  * @date 2018/9/4
  */
 public class SessionConst {
+
     /**
      * 当前登录用户session key
      */
     public static final String USER_SESSION_KEY = "user";
+
     /**
      * 记住我cookie
      */
@@ -20,10 +22,15 @@ public class SessionConst {
      * 管理员强制退出
      */
     public static final String FORCE_LOGOUT = "force_logout";
+
     /**
      * 在其他地方登录,被踢出
      */
     public static final String LOGIN_ELSEWHERE = "login_elsewhere";
 
+    /**
+     * 登录验证码
+     */
+    public static final String VERIFICATION_CODE = "verificationCode";
 
 }
--- easy-admin/src/main/java/com/frame/easy/common/constant/SysConfigConst.java
+++ easy-admin/src/main/java/com/frame/easy/common/constant/SysConfigConst.java
@@ -11,22 +11,22 @@ public class SysConfigConst {
      * 登录时密码错误尝试次数，超过后会被账号会被锁定
      */
     public static final String LOGIN_ATTEMPTS = "loginAttempts";
+
     /**
      * 尝试登录次数过多账号锁定时长 单位：秒
      */
     public static final String LOGIN_LOCK_LENGTH = "loginLockLength";
-    /**
-     * 是否开启验证码
-     */
-    public static final String LOGIN_VERIFICATION_CODE = "loginVerificationCode";
+
     /**
      * 系统名称
      */
     public static final String PROJECT_NAME = "projectName";
+
     /**
      * 系统版本号
      */
     public static final String PROJECT_VERSION = "projectVersion";
+
     /**
      * session失效时间 单位：秒
      */
@@ -36,5 +36,20 @@ public class SysConfigConst {
      * 新增用户时的默认密码
      */
     public static final String DEFAULT_PASSWORD = "defaultPassword";
+
+    /**
+     * 是否开启验证码
+     */
+    public static final String LOGIN_VERIFICATION_CODE = "loginVerificationCode";
+
+    /**
+     * 登录失败累计多少次后需要输入验证码后才可以登录,需要同时开启loginVerificationCode生效
+     */
+    public static final String LOGIN_ATTEMPTS_VERIFICATION_CODE = "loginAttemptsVerificationCode";
+
+    /**
+     * 是否开启用户注册
+     */
+    public static final String OPEN_REGISTRATION = "openRegistration";
 
 }
