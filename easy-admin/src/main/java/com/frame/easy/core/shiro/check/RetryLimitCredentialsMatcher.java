1acbf25d955e7c6eb6a3765adeec98c18d0b1e14
tengchong <tengchongchong@foxmail.com>
2019-03-27 13:41:13 +0800

    优化登录

--- easy-admin/src/main/java/com/frame/easy/core/shiro/check/RetryLimitCredentialsMatcher.java
+++ easy-admin/src/main/java/com/frame/easy/core/shiro/check/RetryLimitCredentialsMatcher.java
@@ -1,10 +1,9 @@
 package com.frame.easy.core.shiro.check;
 
 import cn.hutool.core.util.StrUtil;
+import com.frame.easy.common.constant.CommonConst;
 import com.frame.easy.common.constant.SessionConst;
 import com.frame.easy.common.redis.RedisPrefix;
-import com.frame.easy.config.properties.ProjectProperties;
-import com.frame.easy.exception.EasyException;
 import com.frame.easy.util.RedisUtil;
 import com.frame.easy.util.ShiroUtil;
 import com.frame.easy.modular.sys.model.SysUser;
@@ -12,24 +11,20 @@ import com.frame.easy.modular.sys.service.ShiroService;
 import com.frame.easy.util.file.ImageUtil;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.credential.SimpleCredentialsMatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
- * 添加登录次数验证,防止暴力破解
- * 默认开启,连续输错5次后账号会被锁定10分钟
- * 可在 application.yml 中 修改 project.login.attempts/project.login.lock-length
+ * 凭证匹配器
  *
  * @author tengchong
  * @date 2018/9/6
  */
-public class RetryLimitCredentialsMatcher extends CredentialsMatcher {
+public class RetryLimitCredentialsMatcher extends SimpleCredentialsMatcher {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    @Autowired
-    private ProjectProperties projectProperties;
 
     @Autowired
     private ShiroService shiroService;
@@ -37,30 +32,20 @@ public class RetryLimitCredentialsMatcher extends CredentialsMatcher {
     @Override
     public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
         SysUser sysUser = (SysUser) info.getPrincipals().getPrimaryPrincipal();
-        String loginCountKey = RedisPrefix.ACCOUNT + "login_count_" + sysUser.getUsername();
-        // 登录尝试次数
-        int loginCount = Integer.parseInt(String.valueOf(RedisUtil.get(loginCountKey)));
-        // 剩余尝试次数
-        int retryCount = (projectProperties.getLoginAttempts() - loginCount);
-
-        boolean matches = super.doCredentialsMatch(token, info);
-        if (!matches) {
-            String msg = retryCount <= 0 ? "您的账号被锁定" + projectProperties.getLoginLockLength() / 60 + "分钟！" : "您还剩" + retryCount + "次重试的机会";
-            throw new EasyException("帐号或密码不正确！" + msg);
-        }
-
         // 清空登录计数
-        RedisUtil.del(loginCountKey);
+        RedisUtil.del(RedisPrefix.ACCOUNT + "login_count_" + sysUser.getUsername());
+        RedisUtil.del(RedisPrefix.ACCOUNT + "login_count_" + sysUser.getEmail());
+        RedisUtil.del(RedisPrefix.ACCOUNT + "login_count_" + sysUser.getPhone());
         RedisUtil.del(RedisPrefix.SESSION + "login_count_" + ShiroUtil.getSession().getId().toString());
         // 更新最后登录时间
         shiroService.updateUserLastLoginDate(sysUser.getId());
         // 检查是否允许用户在多处登录
-        if (true) {
+        if (CommonConst.projectProperties.getLoginMultipoint()) {
             shiroService.kickOutSession(sysUser);
         }
         // 用户信息放在session里
         // 设置头像缩略图
-        if(StrUtil.isNotBlank(sysUser.getAvatar())){
+        if (StrUtil.isNotBlank(sysUser.getAvatar())) {
             sysUser.setAvatarLg(ImageUtil.getThumbnailUrlByUrl(sysUser.getAvatar(), ImageUtil.IMAGE_SIZE_LG));
             sysUser.setAvatarMd(ImageUtil.getThumbnailUrlByUrl(sysUser.getAvatar(), ImageUtil.IMAGE_SIZE_MD));
             sysUser.setAvatarSm(ImageUtil.getThumbnailUrlByUrl(sysUser.getAvatar(), ImageUtil.IMAGE_SIZE_SM));
