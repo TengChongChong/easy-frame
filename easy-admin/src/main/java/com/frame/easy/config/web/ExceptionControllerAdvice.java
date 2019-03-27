2574285d5c8a32742ed4af51c35463714d738db1
tengchong <tengchongchong@foxmail.com>
2019-03-27 09:28:11 +0800

    优化异常处理

--- easy-admin/src/main/java/com/frame/easy/config/web/ExceptionControllerAdvice.java
+++ easy-admin/src/main/java/com/frame/easy/config/web/ExceptionControllerAdvice.java
@@ -7,11 +7,14 @@ import com.frame.easy.result.Tips;
 import com.frame.easy.exception.EasyException;
 import com.frame.easy.core.web.Servlets;
 import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authz.UnauthenticatedException;
 import org.apache.shiro.authz.UnauthorizedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -28,51 +31,96 @@ public class ExceptionControllerAdvice {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
+
     /**
-     * 通用异常
-     * 如果是ajax返回json,如果是普通请求返回错误页面
-     *
-     * @param request 请求
-     * @param e       异常
-     * @return Tips/ModelAndView
+     * 拦截业务异常
+     */
+    @ExceptionHandler(EasyException.class)
+    public Object easyException(HttpServletRequest request, EasyException e) {
+        logger.debug("业务异常", e);
+        if (Servlets.isAjaxRequest(request)) {
+            return Tips.getErrorTips(e.getMessage());
+        } else {
+            return errorModelAndView(request.getRequestURI(), e.getCode(), e.getMessage(), e);
+        }
+    }
+
+    /**
+     * 拦截业务异常
+     */
+    @ExceptionHandler(UnauthenticatedException.class)
+    public ModelAndView unauthenticatedException(UnauthenticatedException e) {
+        return new ModelAndView("/login");
+    }
+
+    /**
+     * 登录异常
+     */
+    @ExceptionHandler(AuthenticationException.class)
+    public Object authenticationException(HttpServletRequest request, AuthenticationException e) {
+        logger.debug("登录异常", e);
+        if (Servlets.isAjaxRequest(request)) {
+            if (e.getCause() != null) {
+                logger.error("登录异常:", e.getCause().getMessage());
+                return Tips.getErrorTips(e.getCause().getMessage());
+            } else {
+                logger.error("登录异常:", e.getMessage());
+                return Tips.getErrorTips(e.getMessage());
+            }
+        } else {
+            return errorModelAndView(request.getRequestURI(), HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage(), e);
+        }
+    }
+
+
+    /**
+     * 权限异常
+     */
+    @ExceptionHandler({UnauthorizedException.class})
+    public Object unauthorizedException(HttpServletRequest request, UnauthorizedException e) {
+        logger.debug("权限异常", e);
+        if (Servlets.isAjaxRequest(request)) {
+            return new Tips(HttpStatus.UNAUTHORIZED.value(), "您无权限访问此资源", null);
+        } else {
+            return errorModelAndView(request.getRequestURI(), HttpStatus.UNAUTHORIZED.value(), "您无权限访问此资源", e);
+        }
+    }
+
+
+    /**
+     * 未知异常
      */
     @ExceptionHandler(RuntimeException.class)
     public Object handleException(HttpServletRequest request, RuntimeException e) {
-        e.printStackTrace();
-        Tips tips = new Tips();
-        if (e instanceof UnauthorizedException) {
-            tips.setCode(HttpStatus.UNAUTHORIZED.value());
-            tips.setMessage("您无权限访问此资源");
-        } else if (e instanceof EasyException) {
-            logger.error("业务异常:", e);
-            tips.setCode(((EasyException) e).getCode());
-            tips.setMessage(e.getMessage());
-        } else if (e instanceof AuthenticationException) {
-            tips.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            if (e.getCause() != null) {
-                logger.error("登录异常:", e.getCause().getMessage());
-                tips.setMessage(e.getCause().getMessage());
-            } else {
-                logger.error("登录异常:", e.getMessage());
-                tips.setMessage(e.getMessage());
-            }
-        } else {
-            tips.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            tips.setMessage(e.getMessage());
-        }
+        logger.debug("未知异常", e);
         if (Servlets.isAjaxRequest(request)) {
-            return tips;
+            return Tips.getErrorTips(e.getMessage());
         } else {
-            ModelAndView modelAndView = new ModelAndView();
-            modelAndView.addObject("code", tips.getCode());
-            modelAndView.addObject("message", e.getMessage());
-            modelAndView.addObject("path", request.getRequestURL());
-            modelAndView.addObject("trace", StrUtil.join("\n\t", e.getStackTrace()));
-            // 当前模式是否为开发模式
-            modelAndView.addObject("isDev", CommonConst.projectProperties.getProfilesActive().equals(ProfilesActiveStatus.dev.getProfilesActive()));
-            modelAndView.setViewName("global/" + tips.getCode());
-            return modelAndView;
+            return errorModelAndView(request.getRequestURI(), HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage(), e);
         }
+    }
+
+    /**
+     * 获取错误提示页面信息
+     *
+     * @param url     请求地址
+     * @param code    错误代码
+     * @param message 错误信息
+     * @param e       异常信息
+     * @return ModelAndView
+     */
+    private ModelAndView errorModelAndView(String url, int code, String message, RuntimeException e) {
+        ModelAndView modelAndView = new ModelAndView("global/" + code);
+        modelAndView.addObject("code", code);
+        modelAndView.addObject("message", message);
+        modelAndView.addObject("path", url);
+        // 当前模式是否为开发模式
+        boolean isDev = CommonConst.projectProperties.getProfilesActive().equals(ProfilesActiveStatus.dev.getProfilesActive());
+        modelAndView.addObject("isDev", isDev);
+        if (isDev) {
+            modelAndView.addObject("trace", StrUtil.join("\n\t", e.getStackTrace()));
+        }
+        return modelAndView;
     }
 
 }
