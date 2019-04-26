d41e48c2ada4d3ef804034c039cc1c399d58cadd
tengchong <tengchongchong@foxmail.com>
2019-04-26 17:36:16 +0800

    导入接口

---
+++ easy-core/src/main/java/com/frame/easy/base/service/ImportService.java
@@ -0,0 +1,29 @@
+package com.frame.easy.base.service;
+
+/**
+ * 导入回调
+ *
+ * @author tengchong
+ * @date 2019-04-26
+ */
+public interface ImportService {
+    /**
+     * 导入前回调
+     * 插入正式表之前会调用此方法
+     * 注: 返回false会触发异常回滚
+     *
+     * @param templateId 模板id
+     * @param userId 用户id
+     * @return true/false
+     */
+    boolean beforeImport(Long templateId, Long userId);
+
+    /**
+     * 导入后回调
+     * 插入正式表后会调用此方法
+     * 注: 返回false会触发异常回滚
+     *
+     * @return true/false
+     */
+    boolean afterImport();
+}
