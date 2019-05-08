a54f20ec7a927ab7635a86dc052bdd38b6c38f1b
tengchong <tengchongchong@foxmail.com>
2019-05-08 17:43:55 +0800

    完善导入数据在线编辑

--- easy-system/src/main/java/com/frame/easy/util/office/ImportExportUtil.java
+++ easy-system/src/main/java/com/frame/easy/util/office/ImportExportUtil.java
@@ -2,6 +2,8 @@ package com.frame.easy.util.office;
 
 import cn.hutool.core.date.DateException;
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
+import com.frame.easy.common.constant.CommonConst;
 import com.frame.easy.common.constant.ImportConst;
 import com.frame.easy.exception.EasyException;
 import com.frame.easy.modular.sys.model.SysImportExcelTemplateDetails;
@@ -203,6 +205,88 @@ public class ImportExportUtil {
     }
 
 
+    //====================================== str: 数据类型&长度验证 ======================================/
+    /**
+     * 验证数据类型是否正确
+     * 只验证date、int、long、double类型
+     *
+     * @param data   单元格中内容
+     * @param config 单元格导入规则
+     */
+    public static void verificationData(String data, SysImportExcelTemplateDetails config) {
+        if (ImportExportUtil.isDate(config.getFieldType())) {
+            try {
+                DateUtil.parse(data);
+            } catch (DateException e) {
+                throw new EasyException(config.getTitle() + "必须为常见日期格式;");
+            }
+        } else if (ImportExportUtil.isInteger(config.getFieldType())) {
+            try {
+                Integer.parseInt(data);
+            } catch (NumberFormatException e) {
+                throw new EasyException(config.getTitle() + "必须为整数;");
+            }
+        } else if (ImportExportUtil.isLong(config.getFieldType())) {
+            try {
+                Long.parseLong(data);
+            } catch (NumberFormatException e) {
+                throw new EasyException(config.getTitle() + "必须为整数;");
+            }
+        } else if (ImportExportUtil.isDouble(config.getFieldType())) {
+            try {
+                Double.parseDouble(data);
+            } catch (NumberFormatException e) {
+                throw new EasyException(config.getTitle() + "必须为小数或整数;");
+            }
+        }
+        // 验证数据长度是否符合字段设置
+        verificationLength(data, config);
+    }
+
+    /**
+     * 验证数据长度是否符合字段长度要求
+     *
+     * @param data   单元格数据
+     * @param config 单元格导入规则
+     * @return true/false
+     */
+    private static boolean verificationLength(String data, SysImportExcelTemplateDetails config) {
+        if (StrUtil.isNotBlank(config.getFieldLength())) {
+            if (ImportConst.FIELD_LENGRH_ARBITRARILY.equals(config.getFieldLength())) {
+                return true;
+            } else if (config.getFieldLength().contains(CommonConst.SPLIT)) {
+                // 小数格式,检查小数点前后是否超出限制
+                int integerLength = Integer.parseInt(config.getFieldLength().split(CommonConst.SPLIT)[0]);
+                int decimalLength = Integer.parseInt(config.getFieldLength().split(CommonConst.SPLIT)[1]);
+                String integerStr, decimalStr = null;
+                if (data.contains(CommonConst.DECIMAL_POINT)) {
+                    integerStr = data.substring(0, data.indexOf(CommonConst.DECIMAL_POINT));
+                    decimalStr = data.substring(data.indexOf(CommonConst.DECIMAL_POINT));
+                } else {
+                    integerStr = data;
+                }
+                if (integerStr.length() > integerLength) {
+                    throw new EasyException(config.getTitle() + "整数部分超出限制[" + integerLength + "];");
+                }
+                if (decimalStr != null) {
+                    if (decimalStr.length() > decimalLength) {
+                        throw new EasyException(config.getTitle() + "小数部分超出限制[" + decimalLength + "];");
+                    }
+                }
+            } else {
+                try {
+                    int length = Integer.parseInt(config.getFieldLength());
+                    if (data.length() > length) {
+                        throw new EasyException(config.getTitle() + "长度超出限制[" + config.getFieldLength() + "];");
+                    }
+                } catch (NumberFormatException e) {
+                    // 如果长度不是int就不进行验证
+                }
+            }
+        }
+        return true;
+    }
+
     /**
      * 是否date类型
      *
@@ -254,5 +338,6 @@ public class ImportExportUtil {
             return filedType.contains(ImportConst.FIELD_TYPE_DOUBLE) || filedType.contains(ImportConst.FIELD_TYPE_DECIMAL);
         }
     }
+    //====================================== end: 数据类型验证 ======================================/
 
 }
