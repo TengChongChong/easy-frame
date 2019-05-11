cd3025489efcaca47ac7cf157616e6cef697c9f8
tengchong <tengchongchong@foxmail.com>
2019-05-11 21:36:22 +0800

    添加示例任务

---
+++ easy-sample/src/main/java/com/frame/easy/modular/sample/job/SampleJob.java
@@ -0,0 +1,22 @@
+package com.frame.easy.modular.sample.job;
+
+import cn.hutool.core.date.DateUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * 示例定时任务
+ *
+ * @author tengchong
+ * @date 2019-05-11
+ */
+@Component
+public class SampleJob {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public void sampleJob() {
+        logger.debug("示例定时任务执行了[" + DateUtil.now() + "]");
+    }
+}
