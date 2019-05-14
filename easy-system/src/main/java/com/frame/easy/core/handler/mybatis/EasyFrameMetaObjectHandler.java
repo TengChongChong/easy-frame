18895187264b30145c485493719c5f0b1f106b6b
tengchong <tengchongchong@foxmail.com>
2019-05-14 17:19:07 +0800

    添加自动填充

---
+++ easy-system/src/main/java/com/frame/easy/core/handler/mybatis/EasyFrameMetaObjectHandler.java
@@ -0,0 +1,58 @@
+package com.frame.easy.core.handler.mybatis;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.frame.easy.modular.sys.model.SysUser;
+import com.frame.easy.util.ShiroUtil;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 自动填充
+ * 用于保存/修改的时候自动填充一些属性
+ *
+ * @author tengchong
+ * @date 2019-05-14
+ */
+@Component
+public class EasyFrameMetaObjectHandler implements MetaObjectHandler {
+    /**
+     * 创建人
+     */
+    private final String CREATE_USER = "createUser";
+    /**
+     * 创建时间
+     */
+    private final String CREATE_DATE = "createDate";
+    /**
+     * 编辑人
+     */
+    private final String EDIT_USER = "editUser";
+    /**
+     * 编辑时间
+     */
+    private final String EDIT_DATE = "editDate";
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        SysUser sysUser = ShiroUtil.getCurrentUser();
+        if (sysUser != null) {
+            // 设置创建人&编辑人
+            this.setFieldValByName(CREATE_USER, sysUser.getId(), metaObject);
+            this.setFieldValByName(EDIT_USER, sysUser.getId(), metaObject);
+        }
+        Date now = new Date();
+        this.setFieldValByName(CREATE_DATE, now, metaObject);
+        this.setFieldValByName(EDIT_DATE, now, metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        SysUser sysUser = ShiroUtil.getCurrentUser();
+        if (sysUser != null) {
+            this.setFieldValByName(EDIT_USER, sysUser.getId(), metaObject);
+        }
+        this.setFieldValByName(EDIT_DATE, new Date(), metaObject);
+    }
+}
