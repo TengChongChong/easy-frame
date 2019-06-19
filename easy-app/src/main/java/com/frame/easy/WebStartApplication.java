fbac8444ff664c51f512a7c5958a150546e2e1bc
tengchong <tengchongchong@foxmail.com>
2019-06-19 11:33:41 +0800

    重命名&添加注释

--- easy-app/src/main/java/com/frame/easy/WebStartApplication.java
+++ easy-app/src/main/java/com/frame/easy/WebStartApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 
 /**
- * tomcat容器下启动类
+ * web 容器下启动类
+ * war包方式部署情况下使用
  *
  * @author tengchong
  * @date 2018/11/22
  */
-public class SpringBootStartApplication extends SpringBootServletInitializer {
+public class WebStartApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(Application.class);
